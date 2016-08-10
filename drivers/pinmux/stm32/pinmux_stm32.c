bccb00b9baafeb240be451b99f028b2d94382a5d
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-08-10 18:28:32 -0300

    pinmux/stm32: add support for STM32F4
    Change-Id: Ib1c14fd31abaa2b05a5ab0f7bd1b4a4748f10f84
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- drivers/pinmux/stm32/pinmux_stm32.c
+++ drivers/pinmux/stm32/pinmux_stm32.c
@@ -42,14 +42,36 @@
  */
 static int enable_port(uint32_t port, struct device *clk)
 {
-	clock_control_subsys_t subsys = stm32_get_port_clock(port);
-
 	/* enable port clock */
 	if (!clk) {
 		clk = device_get_binding(STM32_CLOCK_CONTROL_NAME);
 	}
 
+	/* TODO: Merge this and move the port clock to the soc file */
+#ifdef	CONFIG_SOC_SERIES_STM32F1X
+	clock_control_subsys_t subsys = stm32_get_port_clock(port);
+
 	return clock_control_on(clk, subsys);
+
+#elif CONFIG_SOC_SERIES_STM32F4X
+
+	struct stm32f4x_pclken pclken;
+	uint32_t ports_enable[STM32_PORTS_MAX] = {
+		STM32F4X_CLOCK_ENABLE_GPIOA,
+		STM32F4X_CLOCK_ENABLE_GPIOB,
+		STM32F4X_CLOCK_ENABLE_GPIOC,
+		STM32F4X_CLOCK_ENABLE_GPIOD,
+		STM32F4X_CLOCK_ENABLE_GPIOE,
+		STM32F4X_CLOCK_ENABLE_GPIOF,
+		STM32F4X_CLOCK_ENABLE_GPIOG,
+		STM32F4X_CLOCK_ENABLE_GPIOH,
+	};
+	/* AHB1 bus for all the GPIO ports */
+	pclken.bus = STM32F4X_CLOCK_BUS_AHB1;
+	pclken.enr = ports_enable[port];
+
+	return clock_control_on(clk, (clock_control_subsys_t *) &pclken);
+#endif
 }
 
 static int stm32_pin_configure(int pin, int func, int altf)
