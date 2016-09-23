7fdc6aff142d4cbcda77fb51462c381d743fb3e3
Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
2016-09-23 16:58:13 -0300

    drivers/flash: add stm32F4xx flash driver
    TODO: cleanup and write protection
    
    Change-Id: I0f69369013460ce9c14ccffb15ecda7d8d20a101
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- arch/arm/soc/st_stm32/stm32f4/flash_registers.h
+++ arch/arm/soc/st_stm32/stm32f4/flash_registers.h
@@ -17,6 +17,50 @@
 #ifndef _STM32F4X_FLASH_REGISTERS_H_
 #define _STM32F4X_FLASH_REGISTERS_H_
 
+/*******************  Bits definition for FLASH_SR register  ******************/
+#define FLASH_SR_EOP                         ((uint32_t)0x00000001)
+#define FLASH_SR_SOP                         ((uint32_t)0x00000002)
+#define FLASH_SR_WRPERR                      ((uint32_t)0x00000010)
+#define FLASH_SR_PGAERR                      ((uint32_t)0x00000020)
+#define FLASH_SR_PGPERR                      ((uint32_t)0x00000040)
+#define FLASH_SR_PGSERR                      ((uint32_t)0x00000080)
+#define FLASH_SR_BSY                         ((uint32_t)0x00010000)
+
+/*******************  Bits definition for FLASH_CR register  ******************/
+#define FLASH_CR_PG                          ((uint32_t)0x00000001)
+#define FLASH_CR_SER                         ((uint32_t)0x00000002)
+#define FLASH_CR_MER                         ((uint32_t)0x00000004)
+#define FLASH_CR_SNB                         ((uint32_t)0x000000F8)
+#define FLASH_CR_SNB_0                       ((uint32_t)0x00000008)
+#define FLASH_CR_SNB_1                       ((uint32_t)0x00000010)
+#define FLASH_CR_SNB_2                       ((uint32_t)0x00000020)
+#define FLASH_CR_SNB_3                       ((uint32_t)0x00000040)
+#define FLASH_CR_SNB_4                       ((uint32_t)0x00000080)
+#define FLASH_CR_PSIZE                       ((uint32_t)0x00000300)
+#define FLASH_CR_PSIZE_0                     ((uint32_t)0x00000100)
+#define FLASH_CR_PSIZE_1                     ((uint32_t)0x00000200)
+#define FLASH_CR_STRT                        ((uint32_t)0x00010000)
+#define FLASH_CR_EOPIE                       ((uint32_t)0x01000000)
+#define FLASH_CR_LOCK                        ((uint32_t)0x80000000)
+
+/***/
+#define FLASH_FLAG_EOP				FLASH_SR_EOP
+#define FLASH_FLAG_OPERR			FLASH_SR_SOP
+#define FLASH_FLAG_WRPERR			FLASH_SR_WRPERR
+#define FLASH_FLAG_PGAERR			FLASH_SR_PGAERR
+#define FLASH_FLAG_PGPERR			FLASH_SR_PGPERR
+#define FLASH_FLAG_PGSERR			FLASH_SR_PGSERR
+#define FLASH_FLAG_RDERR			((uint32_t)0x00000100)
+#define FLASH_FLAG_BSY				FLASH_SR_BSY
+
+#define FLASH_PSIZE_BYTE			((uint32_t)0x00000000)
+#define FLASH_PSIZE_WORD			((uint32_t)0x00000200)
+#define CR_PSIZE_MASK				((uint32_t)0xFFFFFCFF)
+
+#define FLASH_KEY1				((uint32_t)0x45670123)
+#define FLASH_KEY2				((uint32_t)0xCDEF89AB)
+#define SECTOR_MASK				((uint32_t)0xFFFFFF07)
+
 /**
  * @brief
  *
@@ -69,6 +113,11 @@ static inline void __setup_flash(void)
 	uint32_t tmpreg = 0;
 
 	regs = (struct stm32f4x_flash *) FLASH_R_BASE;
+
+	if (regs->ctrl & FLASH_CR_LOCK) {
+		regs->key = FLASH_KEY1;
+		regs->key = FLASH_KEY2;
+	}
 
 	if (CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC <= 30000000) {
 		regs->acr.bit.latency = STM32F4X_FLASH_LATENCY_0;
